91d4dd713e8ed02356ac573ee53990cf1c25ffa0
zeckyon <rodrigomacielzeck@gmail.com>
2023-09-01 21:54:57 -0300

    iniciado exercicio

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+exercicio 1 aula 16
+
+a)
+testa se o resultado é o suficiente para passar neste "teste"
+
+b)acima de 5
+
+c)se o resto da divisão por 2 é zero
+
+*/
+
+/* 
+exercicio 2 aula 17
+
+a) para falar preço de determinada fruta
+
+b)2,25
+
+c)ele ira continuar a executar o codigo
+*/
+
+/*
+exercicio 3 aula 18
+
+a)definindo que o valor de "numero" é igual ao numero respondido na prompt
+
+b)se for igual a 10 ent será exibido "Esse número passou no teste"
+se for -10 ent deu errado 
+
+c)eu n entendi a pergunta ;-;
+
+*/
+/*
+const numero = Number(prompt("qual idade"))
+if(numero >= 18) {
+    console.log("vc pode dirigir")}
+else {console.log("n pode dirigir")}
+
+*/
+/*
+const turno = prompt(("qual seu turno (entre M, T, N) ").toLowerCase)
+
+if(turno === "M"){console.log("bom dia")}
+
+if(turno === "T"){console.log("boa tarde")}
+
+if(turno === "N"){console.log("boa noite")}
+
+*/
+
+
